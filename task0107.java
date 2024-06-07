8fd6c8045a2c438f4eb954714bf0e0968382c99f
Vitaliy230888 <slam20061@yandex.ru>
2024-06-07 13:06:45 +0300

    Add files via upload

---
+++ task0107.java
@@ -0,0 +1,17 @@
+package task0107;
+
+public class task0107 {
+		public static void main(String[] args) {
+			int x = 2;
+			int y = 12;
+
+			// y = x * y;
+			// y = x + y;
+
+//			x = y - x;
+//			y = y - x;
+
+			System.out.println(y);
+			System.out.println(x);
+		}
+}
